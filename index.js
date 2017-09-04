e4a598d88fa3f3df5e171573deaaa8109c93581a
spagni <sebastianpagni@gmail.com>
2017-09-04 00:22:29 -0300

    Commit changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+require('./services/passport');
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-	res.send({ bye: "buddy" });
-});
+const app = express();
+
+require('./routes/authRoutes')(app);
 
 app.listen(port);
