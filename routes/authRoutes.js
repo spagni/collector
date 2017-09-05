cc5bb435de4b60c842786001aa872d2709da6940
spagni <sebastianpagni@gmail.com>
2017-09-04 23:26:50 -0300

    finished auth flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,22 @@
-const passport = require("passport");
+const passport = require('passport');
 
-module.exports = (app) => {
+module.exports = app => {
 	app.get(
-		"/auth/google",
-		passport.authenticate("google", {
-			scope: ["profile", "email"]
+		'/auth/google',
+		passport.authenticate('google', {
+			scope: ['profile', 'email']
 		})
 	);
 
-	app.get("/auth/google/callback", passport.authenticate("google"));
+	app.get('/auth/google/callback', passport.authenticate('google'));
+
+	app.get('/api/currentUser', (req, res) => {
+		res.send(req.user);
+	});
+
+	app.get('/api/logout', (req, res) => {
+		//PassportJS añade este metodo
+		req.logout();
+		res.send(req.user);
+	});
 };
--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,40 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
 
 passport.use(
 	new GoogleStrategy(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
+			callbackURL: '/auth/google/callback',
+			proxy: true
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken: ' + accessToken);
-			console.log('refreshToken: ' + refreshToken);
-			console.log('profile: ' + JSON.stringify(profile));
+			User.findOne({ googleId: profile.id }).then(existingUser => {
+				if (existingUser) {
+					//El usuario ya existe y no tengo que registrarlo
+					done(null, existingUser);
+				} else {
+					//El usuario no existe y debe ser registrado
+					new User({ googleId: profile.id }).save().then(user => {
+						done(null, user);
+					});
+				}
+			});
 		}
 	)
 );
