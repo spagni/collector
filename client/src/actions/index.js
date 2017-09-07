85c56768ba0903467345c36eea59067bb47e29e5
spagni <spagni@kimn.com.ar>
2017-09-07 18:03:04 -0300

    Commit actions, actionCreator. Uses axios and Redux Thunk

---
+++ client/src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+/*
+Esto es un actionCreator. Normalmente deberia devolver un action (JS Object), pero redux Thunk nos permite acceder directamente al dispatcher
+El parametro dispatch es una function asignada por Thunk una vez que se applica el middleware en src/index.js
+*/
+const fetchUser = () => {
+    return function(dispatch) {
+        axios
+            .get('/api/currentUser')
+            .then(res => dispatch({ type: FETCH_USER, payload: res }));
+    };
+};
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>
-                Hi there Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">
+                        Collector
+                    </a>
+                    <ul className="right">
+                        <li>
+                            <a>Login with Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
