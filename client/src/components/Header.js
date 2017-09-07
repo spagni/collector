85171538595d2766aec1a145e528d94c21e484aa
spagni <spagni@kimn.com.ar>
2017-09-07 16:05:00 -0300

    Commit First Components approach

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                Hi there Header
+            </div>
+        );
+    }
+}
+
+export  default Header;
