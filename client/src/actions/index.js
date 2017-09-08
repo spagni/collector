5ff1fc4b2bb038059dc80d5ab694e273ef19c3db
spagni <spagni@kimn.com.ar>
2017-09-08 17:59:53 -0300

    Commit Dinamic Header based on auth states

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,10 +5,9 @@ import { FETCH_USER } from './types';
 Esto es un actionCreator. Normalmente deberia devolver un action (JS Object), pero redux Thunk nos permite acceder directamente al dispatcher
 El parametro dispatch es una function asignada por Thunk una vez que se applica el middleware en src/index.js
 */
-const fetchUser = () => {
-    return function(dispatch) {
+export const fetchUser = () =>
+    dispatch => {
         axios
             .get('/api/currentUser')
-            .then(res => dispatch({ type: FETCH_USER, payload: res }));
+                .then(res => dispatch({ type: FETCH_USER, payload: res.data }));
     };
-};
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,16 @@
-export default function(state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+
+/*
+Estados de la autenticacion
+null: no sabemos si el usuario está logueado o no. Por ejemplo si tarda mucho el request al server, el estado va a ser null hasta que vuelva
+false: Sabemos que el usuario no está logueado
+User: El usuario está logueado
+Definimos el estado default como null
+*/
+export default function(state = null, action) {
     switch (action.type) {
+        case FETCH_USER:
+            return action.payload || false;
         default:
             return state;
     }
