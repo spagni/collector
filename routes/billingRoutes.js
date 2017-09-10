dd0b1ce6a3e7d9d80f25aa0d343cb8f543d178c2
spagni <sebastianpagni@gmail.com>
2017-09-10 14:51:15 -0300

    Commit Complete FrontEnd Billing - User Validation Middleware

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,16 +1,22 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-	app.post('/api/stripe', (req, res) => {
-    stripe.charges.create({
-      amount: 500,
-      currency: 'USD',
-      description: 'Credits purchase',
-      source: req.body.id
-    })
-      .then(charge => {
-        console.log(charge);
-      });
-  });
+	app.post('/api/stripe', requireLogin, (req, res) => {
+		stripe.charges
+			.create({
+				amount: 500,
+				currency: 'USD',
+				description: 'Credits purchase',
+				source: req.body.id
+			})
+			.then(charge => {
+				//PassportJS hace un append del user al request cuando lo deserializa(middleware) y nosotros lo tenemos disponible en todos los request
+				req.user.credits += 5;
+				req.user.save().then(user => {
+					res.send(user);
+				});
+			});
+	});
 };
