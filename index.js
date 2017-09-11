ddf7f77c3a1dd4aeacbd7ced3ceef1b6c0b65d34
spagni <spagni@kimn.com.ar>
2017-09-11 15:06:42 -0300

    React Heroku server setup. How to deploy to production

--- index.js
+++ index.js
@@ -25,4 +25,15 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === 'production') {
+	//Express will serve up production assets like main.js or main.css
+	app.use(express.static('client/build'));
+
+    //Express will serve up the index.html if it doesn't recognize the route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 app.listen(port);
